6161d6982096d9e8bc55e1e35827e90c683c8780
Abdelmjid-ASOUAB <abde.asouab@gmail.com>
2023-03-31 14:27:18 +0000

    LEVEL-1: Creating Header View

--- app/src/main/java/com/example/cnexia_challenge/MainActivity.kt
+++ app/src/main/java/com/example/cnexia_challenge/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.cnexia_challenge
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.cnexia_challenge.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        this.binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(this.binding.root)
     }
 }
